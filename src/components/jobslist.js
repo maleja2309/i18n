1b7e81e3e164a4f9b9c8fa470889da9e72abb94a
ma.vargas73 <ma.vargas73@uniandes.edu.co>
2023-05-16 09:25:04 -0500

    cambios actividad

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Job from "./job";
+import { FormattedMessage } from 'react-intl';
 
 const JobsList = () => {
   const [offers] = useState([
@@ -10,6 +11,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      visit: '1500'
     },
     {
       id: "0002",
@@ -18,6 +20,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      visit: '4500'
     },
     {
       id: "0003",
@@ -26,20 +29,23 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      visit: '40'
     },
   ]);
 
   return (
     <div>
       <table className="table">
-        <thead className="thead-dark">
+        <thead className={color()}>
+
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col"><FormattedMessage id="Position" /></th>
+            <th scope="col"><FormattedMessage id="Company" /></th>
+            <th scope="col"><FormattedMessage id="Salary" /></th>
+            <th scope="col"><FormattedMessage id="City" /></th>
+            <th scope="col"><FormattedMessage id="PublicationDate" /></th>
+            <th scope="col"><FormattedMessage id="Views" /></th>
           </tr>
         </thead>
         <tbody>
@@ -51,5 +57,16 @@ const JobsList = () => {
     </div>
   );
 };
+
+function color() {
+  color = null;
+  if (navigator.language.toString() != 'es') {
+    color = "table-dark";
+  }
+  else
+    color = "table-light";
+
+  return color;
+}
 
 export default JobsList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider } from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+var lan = 'es';
+var loc = localeEsMessages;
+if (navigator.language.toString() != 'es') {
+    lan = 'en';
+    loc = localeEnMessages;
+};
+
+ReactDOM.render(
+    <IntlProvider locale={lan} messages={loc}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root"));
+
